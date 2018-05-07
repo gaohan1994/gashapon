8c333d2bec19d0ff7a50970232a4efb58c7fde82
gaohan1994 <871418277@qq.com>
2018-05-07 10:06:29 +0800

    用户类加入地址方法 和 通过accesstoken获取id方法

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -1,4 +1,5 @@
 import { getAccessToken } from '../config/util';
+import { NormalReturnObject } from './base';
 
 export interface UserType {
     userId  : string;
@@ -20,6 +21,24 @@ interface Params {
     remain  ?: number;
 }
 
+export interface DoAddressMethodParam {
+    data: {
+        receiver    : string;
+        phone       : string;
+        detail_area : string;
+        detail_home : string;
+        postal_code : string;
+        is_default  ?: boolean;
+    };
+}
+
+export interface GetUserFromAccesstokenRetrun {
+    success ?: boolean;
+    type    ?: string;
+    message ?: string;
+    uid     ?: string;
+}
+
 class User {
     /* id cookie拿 */
     private userId  : string;
@@ -39,8 +58,10 @@ class User {
     constructor () {
         this.userId = getAccessToken();
 
-        this.getUser = this.getUser.bind(this);
-        this.setUser = this.setUser.bind(this);
+        this.getUser                = this.getUser.bind(this);
+        this.setUser                = this.setUser.bind(this);
+        this.doAddAddressMethod     = this.doAddAddressMethod.bind(this);
+        this.doChangeAddressMethod  = this.doChangeAddressMethod.bind(this);
     }
     
     public getUser = (): UserType => {
@@ -63,6 +84,127 @@ class User {
         this.name       = name      ? name : '';
         this.headimg    = headimg   ? headimg : '';
         this.remain     = remain    ? remain : 0;
+    }
+
+    public getUserFromAccesstoken = async (uuid: string): Promise <GetUserFromAccesstokenRetrun> => {
+        try {
+            if (!uuid) {
+                throw new Error('uuid');
+            } 
+        } catch (err) {
+            console.log(err.message);
+            return {
+                type: 'ERROR_GET_USERID',
+                message: err.message ? err.message : '获取用户id错误'
+            };
+        }
+
+        try {
+            
+            const result = await fetch(`/accesstoken/${uuid}`).then(res => res.json());
+
+            if (result.success === true) {
+                return { 
+                    success : true,
+                    uid     : result.result
+                };
+            } else {
+                throw new Error(result.message ? result.message : '获取用户id错误');
+            }
+
+        } catch (err) {
+            console.log('getUserFromAccesstoken', err);
+            return {
+                type: 'FETCH_ACCESSTOKEN_ERROR',
+                message: err.message ? err.message : '获取用户id错误'
+            };
+        }
+    }
+
+    public doAddAddressMethod = async ({data}: DoAddressMethodParam): Promise <NormalReturnObject> => {
+        try {
+            if (!this.userId) {
+                throw new Error('userId');
+            } else if (!data.receiver) {
+                throw new Error('receiver');
+            } else if (!data.phone) {
+                throw new Error('phone');
+            } else if (!data.detail_area) {
+                throw new Error('detail_area');
+            } else if (!data.detail_home) {
+                throw new Error('detail_home');
+            } else if (!data.postal_code) {
+                throw new Error('postal_code');
+            } 
+        } catch (err) {
+            console.log(err.message);
+            return {
+                type: 'ERROR_ADD_ADDRESS',
+                message: err.message ? err.message : '增加地址数据错误'
+            };
+        }
+
+        try {
+
+            const result = await fetch(`/add/address/${this.userId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            }).then(res => res.json());
+
+            if (result.success === true) {
+                return { success: true };
+            } else {
+                throw new Error(result.message ? result.message : '增加地址出错');
+            }
+
+        } catch (err) {
+            console.log('doAddAddressMethod', err);
+            return {
+                type: 'FETCH_ADD_ADDRESS_ERROR',
+                message: err.message ? err.message : '增加地址出错'
+            };
+        }
+    }
+
+    public doChangeAddressMethod = async ({data}: DoAddressMethodParam): Promise <NormalReturnObject> => {
+        try {
+            if (!this.userId) {
+                throw new Error('userId');
+            }
+        } catch (err) {
+            console.log(err.message);
+            return {
+                type: 'ERROR_CHANGE_ADDRESS',
+                message: err.message ? err.message : '修改地址数据错误'
+            };
+        }
+
+        try {
+
+            const result = await fetch(`/change/address/${this.userId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            }).then(res => res.json());
+
+            if (result.success === true) {
+                return { success: true };
+            } else {
+                throw new Error(result.message ? result.message : '修改地址数据错误');
+            }
+            
+        } catch (err) {
+            console.log('doChangeAddressMethod', err);
+            return {
+                type: 'FETCH_CHANGE_ADDRESS_ERROR',
+                message: err.message ? err.message : '修改地址数据错误'
+            };
+        }
     }
 }
 
